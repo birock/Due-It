02862b684bb60f7c74ac336379e96b4b3bfac92f
dmckenna24 <d.mckenna24@gmail.com>
2018-11-18 15:11:22 -0500

    I hope this works!

---
+++ Due It/Task.swift
@@ -0,0 +1,83 @@
+//
+//  Task.swift
+//  Due It
+//
+//  Created by Dan McKenna on 11/18/18.
+//  Copyright © 2018 Due It. All rights reserved.
+//
+
+import UIKit
+
+class Task: NSObject {
+    var taskName: String; //name of the task itself
+    var urgerncyCode: Int; //how important the task is
+    var dateCode: Int; //how close is the task to the due date?
+    var taskCode: Int;
+    var dueDate: Date; //when the task needs to be completed by
+    var taskColor: UIColor; //the color that the task has to be according to date and urgency
+    
+    /* initialize the task with a name, urgency, and due date from the user.
+     * Then init the date code and color generically for now.
+     */
+    init(name: String, urgency: Int, date: Date) {
+        self.taskName = name;
+        self.urgerncyCode = urgency;
+        self.dueDate = date;
+        self.dateCode = 0;
+        self.taskCode = 0;
+        self.taskColor = UIColor.green;
+    }
+    
+    func getTaskCode() -> Int{
+        //the code to determine the color is based on the urgency + date
+        taskCode = (self.urgerncyCode + self.dateCode);
+        return taskCode;
+    }
+    
+    func getDateCode() -> Int{
+        //retrieve the amount of days between now and the due date, so everytime this
+        // method is called it returns the updated code and updates the field in the object itself.
+        let daysBetween = Calendar.current.component(.day, from: self.dueDate);
+        
+        if(daysBetween >= 14){
+            self.dateCode = 1;
+        }
+        else if(daysBetween >= 7){
+            self.dateCode = 2;
+        }
+        else if(daysBetween >= 5){
+            self.dateCode = 3;
+        }
+        else if(daysBetween >= 3){
+            self.dateCode = 4;
+        }
+        else{
+            self.dateCode = 5;
+        }
+        
+        return self.dateCode;
+    }
+    
+    func getTaskColor() -> UIColor{
+        /*
+        * Ben, for this I'm thinking of coming up with an array of 10 preset colors based on task code.
+        * That way, you can save the RBG or Hex values, and quickly return them in this method. Let me know
+        * what you think!
+        * -- Dan
+        */
+        
+        return UIColor.green;
+    }
+    
+    func getTaskName() -> String{
+        return self.taskName;
+    }
+    
+    func getDueDate() -> Date{
+        return self.dueDate;
+    }
+    
+    
+    
+    
+}
---
+++ Due It/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Due It
+//
+//  Created by Dan McKenna on 11/12/18.
+//  Copyright © 2018 Due It. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
