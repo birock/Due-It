a7fa703937f9134b98aaa271ddc29e3280fc6dbb
Ben Rock <bir224@nyu.edu>
2018-11-25 00:53:54 -0500

    Functioning to-do list

--- Due It/AddTaskViewController.swift
+++ Due It/AddTaskViewController.swift
@@ -11,13 +11,34 @@ import UIKit
 class AddTaskViewController: UIViewController {
     
     
+    @IBOutlet weak var input: UITextField!
+    
+    
+    @IBAction func add_item(_ sender: Any) {
+        
+        if (input.text != "") {
+            list.append(input.text!)
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+
+        // Do any additional setup after loading the view.
     }
     
     
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
 }
-
-
--- Due It/ViewController.swift
+++ Due It/ViewController.swift
@@ -8,16 +8,22 @@
 
 import UIKit
 
+var list = ["Write essay", "Do laundry", "Read chapter 3", "Call mom"];
+
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    var tasks = ["Write essay", "Do laundry", "Read chapter 3", "Call mom"];
+    var tasks = list;
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return (tasks.count);
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
         let task = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: "Task")
+        task.backgroundColor = UIColor(red: 1.0, green: 0, blue: 0, alpha: 0)
+        task.textLabel?.text = tasks[indexPath.row]
+
         return(task)
     }
     
@@ -26,6 +32,10 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             self.tasks.remove(at: indexPath.row)
             myTableView.reloadData()
         }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        myTableView.reloadData()
     }
     
 
